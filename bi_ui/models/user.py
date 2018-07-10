28ccd6da3867e541719df281c815f3d20aa76249
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-07-10 08:34:44 +0100

    Remove commented out code from user model

--- bi_ui/models/user.py
+++ bi_ui/models/user.py
@@ -5,16 +5,3 @@ class User(UserMixin):
 
     def __init__(self, user_id):
         self.id = user_id
-
-# class User():
-#     def is_authenticated(self):
-#         return True
-#
-#     def is_active(self):
-#         return True
-#
-#     def is_anonymous(self):
-#         return False
-#
-#     def get_id(self):
-#         return '123-abc'
