bf0e065f3d982263e1adb1958e571dea7eb36ace
Tom Cooling <Tom.Cooling@ons.gov.uk>
2018-07-16 14:38:27 +0100

    Add starter code for implementing pagination
    - Ensure user can navigate to the results page without having done a search

--- bi_ui/routes/api.py
+++ bi_ui/routes/api.py
@@ -1,5 +1,7 @@
 import logging
-from flask import Blueprint, request, flash, redirect, url_for
+from math import ceil
+from structlog import wrap_logger
+from flask import Blueprint, request, flash, redirect, url_for, session
 from flask_login import login_required
 
 from bi_ui.services.business_service import BusinessService
@@ -9,7 +11,7 @@ from bi_ui.utilities.convert_bands import employment_bands, legal_status_bands, 
 from bi_ui.utilities.helpers import compose, convert_band, highlight
 
 
-logger = logging.getLogger(__name__)
+logger = wrap_logger(logging.getLogger(__name__))
 
 
 api_bp = Blueprint('api_bp', __name__, static_folder='static', template_folder='templates')
@@ -27,18 +29,33 @@ turnover_band = lambda business: convert_band(business, 'Turnover', 'turnover ba
 
 @api_bp.route('/search_businesses', methods=['POST'])
 @login_required
-def search_businesses():
-    business_name = request.form['BusinessName']
+def search_businesses(business_name=None, fro=0, size=5):
+    if not business_name:
+        business_name = request.form['BusinessName']
+    query = f'BusinessName:{business_name}&from={fro}&size={size}'
     try:
-        num_results, json = business_service.search_businesses(f'BusinessName:{business_name}')
+        num_results, json = business_service.search_businesses(query)
     except (ApiError, ValueError) as e:
         logger.error('Unable to return Business search results')
         raise e
+
+    # Save the query in the session, so we can implement pagination
+    session['business_name'] = business_name
 
     convert_bands = compose(sic, trading_status, legal_status, employment_band, turnover_band)
     highlighted = [highlight(business, business_name) for business in json]
     businesses = list(map(convert_bands, highlighted))
 
     # We will implement pagination later, for now we can just pass a subset of the results
-    flash([num_results, businesses[0:5]])
+    flash([num_results, businesses])
     return redirect(url_for('results_bp.results'))
+
+
+@api_bp.route('/next_page', methods=['POST'])
+@login_required
+def next_page():
+    business_name = session.get('business_name')
+    fro = int(session['from']) + 5
+    size = int(session['size']) + 5
+    return search_businesses(business_name=business_name, fro=fro, size=size)
+
